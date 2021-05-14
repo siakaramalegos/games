cc3e484d8e221f9a6da4fd2d0afdb1544bb0449a
Sia Karamalegos <siakaramalegos@gmail.com>
2021-05-14 16:56:49 -0500

    Switch to scraping for complexity ratings

--- src/_data/games.js
+++ src/_data/games.js
@@ -1,9 +1,8 @@
 const fs = require('fs')
 const fetch = require('node-fetch')
+const puppeteer = require('puppeteer');
 const BGG_USERNAME = "nolasia"
 const BASE_URL = 'https://bgg-json.azurewebsites.net/'
-const CACHE_FILE_PATH = '_cache/games.json'
-const extraRatings = require("./extraRatings")
 
 async function fetchGames() {
   const url = `${BASE_URL}collection/${BGG_USERNAME}`
@@ -12,13 +11,6 @@ async function fetchGames() {
   if (response.ok) {
     const games = await response.json()
 
-    console.log(games.map(g => {
-      return {
-        gameId: g.gameId,
-        name: g.name,
-      }
-    }));
-
     console.log(`>>> ${games.length} games fetched from ${url}`)
     return games
   }
@@ -26,19 +18,24 @@ async function fetchGames() {
   return null
 }
 
-async function fetchGame(id, complexityRating) {
-  const url = `${BASE_URL}thing/${id}`
+async function fetchGame(game) {
+  const { gameId, complexityRating } = game
+  const url = `${BASE_URL}thing/${gameId}`
   const response = await fetch(url)
 
   if (response.ok) {
-    const game = await response.json()
-    console.log(`>>> ${game.name} data fetched from ${url}`)
-    return {
-      ...game,
-      complexityRating,
+    const data = await response.json()
+    data.complexityRating = complexityRating
+
+    if (hasValidPollResults(data.playerPollResults)) {
+      data.bestNumPlayers = recommendedNumPlayers(data.playerPollResults)
     }
+
+    console.log(`>>>> ${data.name} data fetched from ${url}`)
+    return data
   }
 
+  console.error({gameId, status: response.status});
   return null
 }
 
@@ -69,48 +66,56 @@ function hasValidPollResults(playerPollResults) {
   return !invalid
 }
 
-// Store expansions inside main games
-async function mungeGames(games) {
-  const gamesWithComplexityRatings = games.map(game => {
-    const ratingsData = extraRatings.filter(g => g.gameId === game.gameId)[0]
+function getRating(complexities, id) {
+  const item = complexities.find(game => game.gameId === id)
+  return item ? item.complexity : "Unavailable"
+}
 
+// Store expansions inside main games
+async function mungeGames(cachedGames, currentBasicGames, addedIds, deletedIds, complexities) {
+  const currentBasicGamesWithComplexities = currentBasicGames.map(game => {
     return {
       ...game,
-      complexityRating: ratingsData.complexityRating
+      complexityRating: getRating(complexities, game.gameId)
     }
   })
+  const addedBaseGames = currentBasicGamesWithComplexities.filter(game => !game.isExpansion && addedIds.has(game.gameId))
+  console.log(`Adding ${addedBaseGames.length} base games...`);
+  const addedDetailedBaseGames = await getDetailedGames(addedBaseGames)
+  // Merge base games, and filter out any games with bad responses
+  const newDetailedBaseGames = [
+    ...cachedGames.map(game => {
+      return {
+        ...game,
+        complexityRating: getRating(complexities, game.gameId)
+      }
+    }),
+    ...addedDetailedBaseGames.filter(game => !!game),
+  ]
 
-  const baseGames = gamesWithComplexityRatings.filter(game => !game.isExpansion)
-  const myExpansionIds = gamesWithComplexityRatings.filter(game => game.isExpansion).map(game => game.gameId)
+  // No need to check for added since we already get all the data available when requesting the collection in currentBasicGamesWithComplexities
+  const myExpansionIds = currentBasicGamesWithComplexities.filter(game => game.isExpansion).map(game => game.gameId)
 
-  const detailedGames = await Promise.all(baseGames.map(async (game) => {
-    return await fetchGame(game.gameId, game.complexityRating)
-  }))
-
-  const detailedWithExpansions =  detailedGames.map(game => {
+  const mungedWithExpansions =  newDetailedBaseGames.map(game => {
     const { expansions, playerPollResults, ...rest } = game
-    let modifiedGame = { ...rest }
+    let modifiedGame = { ...rest, expansions }
 
     if (expansions) {
-      const detailedExpansions = expansions.filter(game => {
+      const myBasicExpansions = expansions.filter(game => {
         return myExpansionIds.includes(game.gameId)
       })
-      if (detailedExpansions.length > 0) {
-        modifiedGame.expansions = detailedExpansions.map(expansion => {
-          return gamesWithComplexityRatings.find(element => element.gameId === expansion.gameId)
+      if (myBasicExpansions.length > 0) {
+        modifiedGame.myExpansions = myBasicExpansions.map(expansion => {
+          return currentBasicGamesWithComplexities.find(element => element.gameId === expansion.gameId)
         })
       }
-    }
-
-    if (hasValidPollResults(playerPollResults)) {
-      modifiedGame.bestNumPlayers = recommendedNumPlayers(game.playerPollResults)
     }
 
     return modifiedGame
   })
 
   // Sort from highest complexity to lowest
-  return detailedWithExpansions.sort((a, b) => {
+  return mungedWithExpansions.sort((a, b) => {
     if (a["complexityRating"] > b["complexityRating"]) {
       return -1;
     }
@@ -122,8 +127,8 @@ async function mungeGames(games) {
   })
 }
 
-// Save games to a data file
-function writeToFile(data) {
+// Save data to a file
+function writeToFile(filePath, data) {
   const dir = '_cache'
   // create cache folder if it doesnt exist already
   if (!fs.existsSync(dir)) {
@@ -133,16 +138,16 @@ function writeToFile(data) {
   const fileContent = JSON.stringify(data, null, 2)
 
   // write data to cache json file
-  fs.writeFile(CACHE_FILE_PATH, fileContent, err => {
+  fs.writeFile(filePath, fileContent, err => {
     if (err) throw err
-    console.log(`>>> games saved to ${CACHE_FILE_PATH}`)
+    console.log(`>>> games saved to ${filePath}`)
   })
 }
 
 // get cache contents from json file
-function readFromCache() {
-  if (fs.existsSync(CACHE_FILE_PATH)) {
-    const cacheFile = fs.readFileSync(CACHE_FILE_PATH)
+function readFromCache(filePath) {
+  if (fs.existsSync(filePath)) {
+    const cacheFile = fs.readFileSync(filePath)
     return JSON.parse(cacheFile)
   }
 
@@ -150,21 +155,118 @@ function readFromCache() {
   return []
 }
 
-module.exports = async function () {
-  const cache = readFromCache()
+function checkDifferences(cached, current) {
+  const currentIds = new Set(current.map(game => game.gameId))
 
-  // Only fetch games in production
-  if (process.env.NODE_ENV === 'production') {
-    console.log('>>> Checking for games...');
-    const games = await fetchGames()
-
-    if (games) {
-      const mungedGames = await mungeGames(games)
-
-      writeToFile(mungedGames)
-      return mungedGames
+  if (cached.length === 0) {
+    return {
+      addedIds: currentIds,
+      deletedIds: [],
     }
   }
 
-  return cache
+  const cachedIds = new Set(cached.map(game => game.gameId))
+  const addedIds = new Set([...currentIds].filter(x => !cachedIds.has(x)));
+  const deletedIds = new Set([...cachedIds].filter(x => !currentIds.has(x)));
+
+  return {
+    addedIds,
+    deletedIds,
+  }
+}
+
+async function getDetailedGames(games) {
+  return await Promise.all(games.map(async (game) => {
+    return await fetchGame(game)
+  }))
+}
+
+async function scrapeForRatings(addedIds) {
+  const IGNORED_ASSETS = ["font", "image", "style"]
+  console.log(">>> Beginning to scrape for complexity ratings");
+  console.log({addedIds});
+
+  console.log("Launching browser...");
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+
+  const complexities = await Promise.all(addedIds.map(async (id) => {
+    const page = await browser.newPage();
+    await page.setRequestInterception(true);
+    page.on('request', (req) => {
+      if (IGNORED_ASSETS.includes(req.resourceType())){
+        req.abort();
+      }
+      else {
+        req.continue();
+      }
+    });
+    await page.goto(`https://boardgamegeek.com/boardgame/${id}`);
+    // Wait for the target node to render
+    await page.waitForSelector("[item-poll-button=boardgameweight] span")
+    const complexity = await page.evaluate(() => {
+      return document.querySelector("[item-poll-button=boardgameweight] span").innerText
+    })
+    console.log(`>>> ${id} scraped complexity is ${complexity}`)
+    return {
+      gameId: id,
+      complexity,
+    }
+  }))
+  await browser.close()
+
+  console.log({complexities});
+  return complexities
+}
+
+async function getComplexities(cachedComplexities, currentBasicGames) {
+  // Don't care about deleted ones
+  const differences = checkDifferences(cachedComplexities, currentBasicGames)
+  const addedIds = Array.from(differences.addedIds)
+
+  if (addedIds.length === 0) {
+    return cachedComplexities
+  }
+
+  const LIMIT = 10
+  const addedRatings = await scrapeForRatings(addedIds.slice(0, LIMIT))
+  return [
+    ...cachedComplexities,
+    ...addedRatings,
+  ]
+}
+
+
+module.exports = async function () {
+  const cachedGames = readFromCache("_cache/games.json")
+  // Use cached games
+  if (process.env.NODE_ENV !== 'production') {
+    return cachedGames
+  }
+
+  // Only fetch games in production
+  console.log('>>> Checking for games...');
+  const currentBasicGames = await fetchGames()
+
+  if (!currentBasicGames) {
+    console.log("No games found.");
+    return cachedGames
+  }
+
+  // Read complexity ratings and fetch any unscraped ones
+  const cachedComplexities = readFromCache("_cache/complexities.json")
+  const newComplexities = await getComplexities(cachedComplexities, currentBasicGames)
+  writeToFile("_cache/complexities.json", newComplexities)
+
+  // Can ignore expansions since no additional requests will be needed to populate them
+  const {addedIds, deletedIds} = checkDifferences(cachedGames, currentBasicGames.filter(game => !game.isExpansion))
+  console.log({addedIds, deletedIds});
+
+  const mungedGames = await mungeGames(cachedGames, currentBasicGames, addedIds, deletedIds, newComplexities)
+  writeToFile("_cache/games.json", mungedGames)
+
+  console.log("Done!");
+
+  return mungedGames
 }
